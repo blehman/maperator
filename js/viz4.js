a914667bdeef48177f8229043b9dfe5c49e9fc2f
Brian Lehman <the.lehman.report@gmail.com>
2015-03-18 17:08:20 -0600

    messing with jsonp for oembed w/ instagram

--- js/viz4.js
+++ js/viz4.js
@@ -43,6 +43,8 @@ function addPoints(x,map){
                 radius: 10
             }).addTo(map);
 
+            var output d3.jsonp("http://api.instagram.com/oembed?url="+d.properties.link+"&callback=fuckJsonP")
+
             var circleData = d3.selectAll(".tweet_location_pre_data")
                 .classed("tweet_location_pre_data",false)
                 .classed("tweet_location " + dateTag,true)
@@ -230,6 +232,37 @@ function create_timeline(data,mapWidth,dateParsed){
 
     }
 }
+
+// d3.jsonp code from https://github.com/d3/d3-plugins/tree/master/jsonp
+
+d3.jsonp = function (url, callback) {
+  function rand() {
+    var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+      c = '', i = -1;
+    while (++i < 15) c += chars.charAt(Math.floor(Math.random() * 52));
+    return c;
+  }
+
+  function create(url) {
+    var e = url.match(/callback=d3.jsonp.(\w+)/),
+      c = e ? e[1] : rand();
+    d3.jsonp[c] = function(data) {
+      callback(data);
+      delete d3.jsonp[c];
+      script.remove();
+    };
+    return 'd3.jsonp.' + c;
+  }
+
+  var cb = create(url),
+    script = d3.select('head')
+    .append('script')
+    .attr('type', 'text/javascript')
+    .attr('src', url.replace(/(\{|%7B)callback(\}|%7D)/, cb));
+};
+
+function fuckJsonP(datas) { console.log(datas);}
+
 
 d3.json("data/BoulderFlood_viewer.json", function(collection) {
   console.log(["collection:",collection])
